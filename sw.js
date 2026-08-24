7a59b25c423b192a77b568625dafee5a9f693006
guest666-code <efemertdede9@gmail.com>
2026-08-25 02:23:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,37 @@
-const CACHE_NAME = 'purpleguy-pwa-v1';
+const CACHE_NAME = 'purpleguy-pwa-v2';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/assets/css/style.css',
-  '/assets/js/main.js',
-  '/manifest.json'
+  './',
+  './index.html',
+  './assets/css/style.css',
+  './assets/js/main.js',
+  './manifest.json',
+  './assets/images/favicon.ico'
 ];
 
-// Yükleme Aşaması
+// Yükleme Aşaması (Dosyaları Önbelleğe Al)
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+// Etkinleştirme Aşaması (Eski Cache'leri Temizle)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 // Çevrimdışı Çalışma / Cache'den Çekme
@@ -24,4 +42,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
